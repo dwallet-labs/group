83532e4747bac3ae065109ff096927b9d3699b01
Yehonatan Cohen Scaly <scalys7@gmail.com>
2024-01-24 21:42:56 +0200

    helpers: Create Module for Misc. Functionality, Implement Const Generic Serialization and `FlatMapResults` (#8)

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,8 @@
 // Author: dWallet Labs, LTD.
 // SPDX-License-Identifier: BSD-3-Clause-Clear
 
+pub mod helpers;
+
 use core::fmt::Debug;
 use core::iter;
 use core::ops::{Add, AddAssign, Mul, Neg, Sub, SubAssign};
